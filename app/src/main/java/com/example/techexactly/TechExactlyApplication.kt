66b2320140c010f49f476163e7eb77b0e0566827
Chayan Pal <chayan110201@gmail.com>
2025-04-05 19:04:44 +0530

    feat: Enhance splash screen and add user search functionality
    This commit enhances the app's splash screen with a custom exit animation and introduces the ability to search for users within the app.
    
    Changes:
    
    -   **Splash Screen Animation:**
        -   Implemented a custom exit animation for the splash screen that zooms out and rotates the icon, using `ObjectAnimator` and `OvershootInterpolator`.
        -   Added `Theme.App.Starting` style for splash screen configuration, including background color, icon background color, and animated icon.
        - Added  `logo_animator.xml` and `animated_logo.xml` to add animation in splash screen icon
        -  Added new `ic_launcher_foreground` in `mipmap` folders and `ic_launcher_background.xml`
    - Added new `app_logo.xml` in `drawable` folder
        -   Updated `ic_launcher.xml` and `ic_launcher_round.xml` in `mipmap-anydpi-v26` to remove deprecated drawables.
        - updated `AndroidManifest.xml` to use splash screen theme
        - Updated `themes.xml` to add splash screen style and `themes.xml (v31)`
    - updated all `ic_launcher` and `ic_launcher_round` to webp format in all `mipmap` folders
    - Added `ic_launcher-playstore.png`
    
    -   **User Search Functionality:**
        -   Added `searchQuery`, `allUsers` state flows to `MainViewModel` to manage user search.
        -   Implemented `setSearchQuery` to update the search query and filter users.
        -   Implemented `filterUsers` to filter the `allUsers` based on the current search query, updating the `uiState` accordingly.
        -   Modified `HomeScreenTopBar` to include a search bar and search icon.
        - Added search functionality and animated icon in `HomeScreenTopBar`
        -   Added `DockedSearchBar` to `HomeScreenTopBar` for user search.
        - Added `clearSearchQuery` action in `HomeScreenTopBar`
        -  Added `BackHandler` in `HomeScreen` to clear the search field when user back press.
        -   Updated `UserList` to display a message when no users are found, including an error icon.
        - Display `UserListFooter` only if there are users in the list
        - Changed `bs` title in `UserDetailsItemCompanyRow` to `Business`
        - Added `Icon` in case of error in `UserList`
        - Updated `HomeScreenContent` to remove `contentAlignment = Alignment.Center`
        - Added `TechExactlyApplication` to apply Koin configuration

--- app/src/main/java/com/example/techexactly/TechExactlyApplication.kt
+++ app/src/main/java/com/example/techexactly/TechExactlyApplication.kt
@@ -3,12 +3,15 @@ package com.example.techexactly
 import android.app.Application
 import com.example.techexactly.di.appModule
 import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 
 class TechExactlyApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         startKoin {
+            androidLogger(Level.DEBUG)
             androidContext(this@TechExactlyApplication)
             modules(appModule)
         }
--- app/src/main/java/com/example/techexactly/view/home/HomeScreenTopBar.kt
+++ app/src/main/java/com/example/techexactly/view/home/HomeScreenTopBar.kt
@@ -1,38 +1,143 @@
 package com.example.techexactly.view.home
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.DockedSearchBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.techexactly.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreenTopBar(
-    title: String, subtitle: String, scrollBehavior: TopAppBarScrollBehavior
+    title: String, subtitle: String,
+    scrollBehavior: TopAppBarScrollBehavior,
+    searchQuery: String = "",
+    onSearchQueryChanged: (String) -> Unit,
+    clearSearchQuery: () -> Unit,
 ) {
-    CenterAlignedTopAppBar(
-        title = {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
+    var showSearchBar by remember { mutableStateOf(false) }
+    Column {
+        CenterAlignedTopAppBar(
+            title = {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = title, maxLines = 1, overflow = TextOverflow.Ellipsis
+                )
+                Text(
+                    text = subtitle, style = MaterialTheme.typography.labelMedium
+                )
+            }
+        }, actions = {
+            AnimatedVisibility(visible = showSearchBar) {
+                IconButton(
+                    onClick = { showSearchBar = !showSearchBar }) {
+                    Icon(
+                        imageVector = Icons.Default.Close, contentDescription = "Close"
+                    )
+                }
+            }
+            AnimatedVisibility(visible = !showSearchBar) {
+                IconButton(
+                    onClick = { showSearchBar = !showSearchBar }) {
+                    Icon(
+                        imageVector = Icons.Default.Search, contentDescription = "Search"
+                    )
+                }
+            }
+        }, navigationIcon = {
+                Image(
+                    modifier = Modifier.padding(start = 8.dp).size(24.dp),
+                    painter = painterResource(id = R.drawable.app_logo),
+                    contentDescription = null,
+                    colorFilter = ColorFilter.tint(
+                        MaterialTheme.colorScheme.onSurface
+                    )
+                )
+            },
+            scrollBehavior = scrollBehavior
+        )
+        AnimatedVisibility(visible = showSearchBar) {
+            DockedSearchBar(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 8.dp, vertical = 4.dp),
+                inputField = {
+                    SearchBarDefaults.InputField(
+                        modifier = Modifier.fillMaxWidth(0.9f),
+                        query = searchQuery,
+                        onQueryChange = {
+                            onSearchQueryChanged(it)
+                        },
+                        onSearch = {
+                            showSearchBar = false
+                        },
+                        expanded = false,
+                        onExpandedChange = { },
+                        placeholder = { Text("Search Users") },
+                        leadingIcon = {
+                            Icon(
+                                Icons.Default.Search, contentDescription = null
+                            )
+                        },
+                        trailingIcon = {
+                            if (showSearchBar) {
+                                IconButton(onClick = {
+                                    if (searchQuery.isNotEmpty()) {
+                                        clearSearchQuery()
+                                    }
+                                    showSearchBar = false
+                                }) {
+                                    Icon(Icons.Default.Close, contentDescription = null)
+                                }
+                            }
+
+                        })
+                },
+                expanded = false,
+                onExpandedChange = {}) {}
+        }
+        AnimatedVisibility(searchQuery.isNotEmpty()) {
             Text(
-                text = title, maxLines = 1, overflow = TextOverflow.Ellipsis
-            )
-            Text(
-                text = subtitle, style = MaterialTheme.typography.labelMedium
+                text = "Showing results for: $searchQuery",
+                style = MaterialTheme.typography.labelMedium,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 8.dp, vertical = 4.dp),
+                textAlign = TextAlign.Center
             )
         }
-    }, navigationIcon = {}, scrollBehavior = scrollBehavior
-    )
+    }
 }
 
 @Preview
@@ -42,6 +147,8 @@ fun HomeScreenTopBarPreview() {
     HomeScreenTopBar(
         title = stringResource(id = R.string.app_name),
         subtitle = "Users",
-        scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
-    )
+        scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(),
+        searchQuery = "",
+        onSearchQueryChanged = {},
+        clearSearchQuery = {})
 }
